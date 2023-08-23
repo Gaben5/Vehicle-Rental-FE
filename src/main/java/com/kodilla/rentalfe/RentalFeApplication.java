1eb89cfcf4d464e34190827fa575301ec2b1426e
Gabriel Borkowski <gabriell.borkowski@gmail.com>
2023-08-23 22:18:15 +0200

    first commit

---
+++ src/main/java/com/kodilla/rentalfe/RentalFeApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.rentalfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalFeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kodilla/rentalfe/RentalFeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.rentalfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalFeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
